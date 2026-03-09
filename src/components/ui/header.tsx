ad8f5df0fc58650c2cbc08d3823846ff81327ee5
Chapiuskys <gabrielmp2709@gmail.com>
2026-03-09 18:46:31 -0300

    feat: add deals page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,38 +1,46 @@
 "use client";
 
-import { Card } from "./card";
-import { Button } from "./button";
 import {
-  LogInIcon,
-  MenuIcon,
-  CirclePercentIcon,
-  ShoppingCartIcon,
-  ListOrderedIcon,
+  Heart,
   HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
   LogOutIcon,
+  MenuIcon,
+  PackageSearchIcon,
+  PercentIcon,
+  ShoppingCartIcon,
 } from "lucide-react";
+import { Button } from "./button";
+import { Card } from "./card";
 import {
-  SheetTrigger,
   Sheet,
+  SheetClose,
   SheetContent,
   SheetHeader,
-  SheetClose,
+  SheetTrigger,
 } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Avatar, AvatarImage } from "./avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Separator } from "./separator";
 import Link from "next/link";
-import Image from "next/image";
 import Cart from "./cart";
+import { useContext } from "react";
+import { CartContext } from "@/providers/cart";
 
 const Header = () => {
   const { status, data } = useSession();
 
-  const HandleLoginClick = async () => {
+  const { products } = useContext(CartContext);
+
+  const cartQuantityItems = products.length;
+
+  const handleLoginClick = async () => {
     await signIn();
   };
 
-  const HandleLogOutClick = async () => {
+  const handleLogoutClick = async () => {
     await signOut();
   };
 
@@ -40,14 +48,14 @@ const Header = () => {
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
         <SheetTrigger asChild>
-          <Button size="icon">
+          <Button size="icon" variant="outline">
             <MenuIcon />
           </Button>
         </SheetTrigger>
 
-        <SheetContent side={"left"}>
+        <SheetContent side="left" className="w-[21.875rem]">
           <SheetHeader className="text-left text-lg font-semibold">
-            <h1>Menu</h1>
+            Menu
           </SheetHeader>
 
           {status === "authenticated" && data?.user && (
@@ -61,30 +69,35 @@ const Header = () => {
                   {data.user.image && <AvatarImage src={data.user.image} />}
                 </Avatar>
 
-                <p className="font-medium">{data.user.name}</p>
+                <div className="flex flex-col">
+                  <p className="font-medium">{data.user.name}</p>
+                  <p className="text-sm opacity-75">Boas compras!</p>
+                </div>
               </div>
+
               <Separator />
             </div>
           )}
 
-          <div className="mt-4 flex flex-col gap-3">
+          <div className="mt-4 flex flex-col gap-2">
             {status === "unauthenticated" && (
               <Button
-                onClick={HandleLoginClick}
-                variant={"secondary"}
-                className="w-full"
+                onClick={handleLoginClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
               >
-                <LogInIcon /> Fazer Login
+                <LogInIcon size={16} />
+                Fazer Login
               </Button>
             )}
 
             {status === "authenticated" && (
               <Button
-                onClick={HandleLogOutClick}
-                variant={"secondary"}
-                className="w-full"
+                onClick={handleLogoutClick}
+                variant="outline"
+                className="w-full justify-start gap-2"
               >
-                <LogOutIcon />
+                <LogOutIcon size={16} />
                 Fazer Logout
               </Button>
             )}
@@ -92,30 +105,58 @@ const Header = () => {
             <SheetClose asChild>
               <Link href="/">
                 <Button
-                  variant={"outline"}
-                  className="w-full hover:bg-primary/70"
+                  variant="outline"
+                  className="w-full justify-start gap-2"
                 >
-                  <HomeIcon />
+                  <HomeIcon size={16} />
                   Início
                 </Button>
               </Link>
+            </SheetClose>
 
-              <Button
-                variant={"outline"}
-                className="w-full hover:bg-primary/70"
-              >
-                <CirclePercentIcon />
-                Ofertas
-              </Button>
+            <SheetClose asChild>
+              <Link href="/orders">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PackageSearchIcon size={16} />
+                  Meus Pedidos
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/wish-list">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <Heart size={16} />
+                  Favoritos
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/deals">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PercentIcon size={16} />
+                  Ofertas
+                </Button>
+              </Link>
             </SheetClose>
 
             <SheetClose asChild>
               <Link href="/catalog">
                 <Button
-                  variant={"outline"}
-                  className="w-full hover:bg-primary/70"
+                  variant="outline"
+                  className="w-full justify-start gap-2"
                 >
-                  <ListOrderedIcon />
+                  <ListOrderedIcon size={16} />
                   Catálogo
                 </Button>
               </Link>
@@ -124,29 +165,25 @@ const Header = () => {
         </SheetContent>
       </Sheet>
 
-      <Link href={"/"} className="flex items-center gap-3">
-        {/* A logo entra aqui */}
-        <Image
-          src="/PurpleTechLogo.png"
-          alt="Mascote Techteech Store"
-          width={75}
-          height={75}
-          className="object-contain"
-        />
-
-        <h1 className="text-lg font-bold">
-          <span className="text-primary">PurpleTech</span> Store{" "}
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">FSW</span> Store
         </h1>
       </Link>
 
       <Sheet>
         <SheetTrigger asChild>
-          <Button size="icon">
+          <Button size="icon" variant="outline" className="relative">
+            {cartQuantityItems > 0 && (
+              <span className="absolute right-[calc(-1.25rem/2)] top-[calc(-1.25rem/2)] flex h-6 w-6 items-center justify-center rounded-lg bg-primary text-sm font-bold">
+                {cartQuantityItems}
+              </span>
+            )}
             <ShoppingCartIcon />
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
+        <SheetContent className="w-[350px] lg:w-[600px] lg:max-w-[600px]">
           <Cart />
         </SheetContent>
       </Sheet>
