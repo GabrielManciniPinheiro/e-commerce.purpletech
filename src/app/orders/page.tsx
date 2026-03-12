acfa36f115b10f97933cd01540b6bb16ef1d9a78
Chapiuskys <gabrielmp2709@gmail.com>
2026-03-12 18:27:33 -0300

    fix: ensure the user are loged to view the delivery page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,22 +1,27 @@
+import { Badge } from "@/components/ui/badge";
 import { authOptions } from "@/lib/auth";
+import { prismaClient } from "@/lib/prisma";
 import { PackageSearchIcon } from "lucide-react";
 import { getServerSession } from "next-auth";
-import { Badge } from "@/components/ui/badge";
-import { prismaClient } from "@/lib/prisma";
 import OrderItem from "./components/order-item";
 
 export const dynamic = "force-dynamic";
 
 async function OrderPage() {
-  const user = getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  if (!user) {
-    return <p>Retorne ao Menu inicial e faça login com sua conta do google!</p>;
+  if (!session || !session.user) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center gap-2 p-5">
+        <h2 className="font-bold">Acesso Negado!</h2>
+        <p className="text-sm opacity-60">Faça login para ver seus pedidos</p>
+      </div>
+    );
   }
 
   const orders = await prismaClient.order.findMany({
     where: {
-      userId: (user as any).id,
+      userId: session.user.id,
     },
     include: {
       orderProducts: {
@@ -28,13 +33,10 @@ async function OrderPage() {
   });
 
   return (
-    <div className="flex flex-col gap-8 p-5">
-      <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-        variant="outline"
-      >
+    <div className="p-5 lg:container lg:mx-auto lg:py-10">
+      <Badge>
         <PackageSearchIcon size={16} />
-        Meus pedidos
+        Meus Pedidos
       </Badge>
 
       <div className="mt-5 flex flex-col gap-5">
